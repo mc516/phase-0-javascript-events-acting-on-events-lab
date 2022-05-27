202149b43aa4a64c3d28b12bd3595a9317e6b3a4
Mickey <mickeyc11111@gmail.com>
2022-05-27 18:47:16 -0400

    commit tree f481711854954e7bc19576e61df87b37acb9da02

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumbers, 10);
+  
+    if (right < 360 ) {
+      dodger.style.left = `${right + 1}px`;
+    }
+  }
+function moveDodgerUp() {
+    const upNumbers = dodger.style.bottom.replace("px", "");
+    const up = parseInt(upNumbers, 10);
+
+    if (up < 400) {
+        dodger.style.bottom = `${up + 1}px`;
+    }
+}
+function moveDodgerDown() {
+    const downNumbers = dodger.style.bottom.replace("px", "");
+    const down = parseInt(downNumbers, 10);
+
+    if (down > 0) {
+        dodger.style.bottom = `${down - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e)  {
+    if (e.key === "ArrowDown") {
+        moveDodgerDown();
+    }
+    });
+
+document.addEventListener("keydown", function (e)  {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+    });
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+  
